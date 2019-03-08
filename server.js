049e9b479c4d50583f0b60d920e247499751bf76
Chris Lion <rudachristiano1@gmail.com>
2019-03-08 15:52:33 +0200

    Server Changed

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
+
+
+
+
 
 // Middleware
-app.use(express.static(__dirname + '/public'));
-app.listen(3000, function(){
-  console.log("listen on ")
-});
+//app.use(express.static(__dirname + '/public'));
+//app.listen(3000, function(){
+//  console.log("listen on ")
+//});
 
-app.post('/', function(req, res) {
-  res.end('Success!!!');
-});
+//app.post('/', function(req, res) {
+//  res.end('Success!!!');
+//});
